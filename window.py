db70302df5f5a0113ca931758fe98e58dbe84e6b
Chris McIntosh <rougechampion2002@gmail.com>
2018-01-14 13:53:24 -0600

    temp check in

--- window.py
+++ window.py
@@ -1,70 +1,120 @@
-#!/usr/bin/python3
 import gi
 gi.require_version('Gtk','3.0')
 from gi.repository import Gtk
 import math
-from OpenGL.GL import *
-from OpenGL.GL import shaders
-from OpenGL.GLUT import *
-from OpenGL.GLU import *
-import numpy as np
 import ctypes
 from wikiNode import WikiNode
 import cairo
-
-# Fragment Shader, static red color
-FRAGMENT_SOURCE = '''
-#version 330
-uniform vec4 textColor;
-uniform sampler2D textureUnit;
-
-#if __VERSION__ >= 130
-in vec4 texCoord;
-out vec4 gl_FragColor;
-#else
-varyingvec4 texCoord;
-#endif
-
-void main(){
-   vec4 red = texture(textureUnit, texCoord.st);
-   gl_FragColor = vec4(1.0,1.0,1.0, red.r) * textColor;
-}'''
-
-# Vertex Shader, passthrough
-VERTEX_SOURCE = '''
-#version 330
-uniform mat4 projMatrix;
-uniform mat4 modelViewMatrix;
-// vertex pos
-in vec4 pos;
-// text coord
-in vec2 itexCoord;
-out vec4 otexCoord;
-
-void main(){
-otexCoord = vec4(itexCoord.s, itexCoord.t, 0.0, 1.0);
-gl_Position = projMatrix * modelViewMatrix * vec4(pos.x, pos.y, 0.0,1.0);
-}'''
-
 
 class TreeWindow(Gtk.Window):
    def __init__(self, wikiNode):
       Gtk.Window.__init__(self, title='Wiki Tree - root name')
       self.node = wikiNode
-      self.resize(1280,768)
+
+      self.set_border_width(10)
+
+#      self.resize(1280,768)
       self.set_position(Gtk.WindowPosition.CENTER)
       self.connect('delete-event', Gtk.main_quit)
 
-      #darea = Gtk.GLArea()
-      #darea.set_has_depth_buffer(False)
-      #darea.set_has_stencil_buffer(False)
-      #darea.connect('render',self.render)
+      box_outer = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing=6)
+      self.add(box_outer)
 
-      #self.add(darea)
+#      listbox = Gtk.ListBox()
+#      listbox.set_selection_mode(Gtk.SelectionMode.NONE)
+#      box_outer.pack_start(listbox,True,True,0)
+#
+#      row = Gtk.ListBoxRow()
+#      hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL,spacing=50)
+#
+#      row.add(hbox)
+#
+#      vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+#      hbox.pack_start(vbox,True,True,0)
+#
+#      label1 = Gtk.Label('Automatic Date & Time', xalign=0)
+#      label2 = Gtk.Label('Requires internet access', xalign=0)
+#
+#      vbox.pack_start(label1, True, True,0)
+#      vbox.pack_start(label2, True,True,0)
+#      switch = Gtk.Switch()
+#      switch.props.valign = Gtk.Align.CENTER
+#      hbox.pack_start(switch,False,True,0)
+#
+#      listbox.add(row)
+#
+#      row = Gtk.ListBoxRow()
+#      hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL,spacing=50)
+#      row.add(hbox)
+#
+#      label = Gtk.Label('Enable Automatic Update', xalign=0)
+#
+#      check = Gtk.CheckButton()
+#      hbox.pack_start(label, True,True,0)
+#      hbox.pack_start(check,False,True,0)
+#
+#      listbox.add(row)
+#
+#      row = Gtk.ListBoxRow()
+#      hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+#
+#      row.add(hbox)
+#
+#      label = Gtk.Label('Date Format', xalign=0)
+#      combo = Gtk.ComboBoxText()
+#      combo.insert(0,'0','24-hour')
+#      combo.insert(1,'1','AM/PM')
+#      hbox.pack_start(label,True,True,0)
+#      hbox.pack_start(combo,False,True,0)
+#
+#      listbox.add(row)
+#
+#      listbox_2 = Gtk.ListBox()
+#
+#      items = 'This is a sorted ListBox'.split()
+#
+#      for item in items:
+#         listbox_2.add(ListBoxRowWithData(item))
+#
+#         def sort_func(row_1, row_2, data, notify_destroy):
+#            return row_1.data.lower() > row_2.data.lower()
+#
+#         def filter_func(row,data,notify_destroy):
+#            return False if row.data == 'Fail' else True
+#
+#         listbox_2.set_sort_func(sort_func,None,False)
+#         listbox_2.set_filter_func(filter_func,None,False)
+#
+#         box_outer.pack_start(listbox_2,True,True,0)
+#         listbox_2.show_all()
+#
+      listbox_3 = Gtk.ListBox()
+      listbox_3.set_selection_mode(Gtk.SelectionMode.NONE)
 
-      drawArea = Gtk.DrawingArea()
-      drawArea.connect('draw',self.renderCairo)
-      self.add(drawArea)
+      nodeRow = Gtk.ListBoxRow()
+      listbox_3.add(nodeRow)
+
+      nodeBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL,spacing=10)
+      nodeRow.add(nodeBox)
+
+      rootBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing=10)
+      rootButton = Gtk.Button(self.node.rawPageBody.title.string, xalign=0)
+      rootBox.add(rootButton)
+      nodeBox.add(rootBox)
+
+      box_outer.pack_start(listbox_3,True,True,0)
+      listbox_3.show_all()
+
+      childBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing=10)
+      nodeBox.add(childBox)
+
+      for child in self.node.children:
+         childLabel = Gtk.Button(child.rawPageBody.title.string,xalign=0)
+         childBox.add(childLabel)
+
+#      drawArea = Gtk.DrawingArea()
+#      drawArea.connect('draw',self.renderCairo)
+#      self.add(drawArea)
       self.show_all()
 
    def renderCairo(self, widget, event):
@@ -89,49 +139,8 @@ class TreeWindow(Gtk.Window):
 
       draw_children(self.node, 20,20)
 
-
-
-
-
-      def render(self, area,ctx):
-         ctx.make_current()
-
-         glClearColor(1,1,1,1)
-         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-         VERTEX_SHADER_PROG = shaders.compileShader(VERTEX_SOURCE, GL_VERTEX_SHADER)
-         FRAGMENT_SHADER_PROG = shaders.compileShader(FRAGMENT_SOURCE, GL_FRAGMENT_SHADER)
-
-         self.shader_prog = shaders.compileProgram(VERTEX_SHADER_PROG, FRAGMENT_SHADER_PROG)
-         self.make_triangle()
-
-      def make_triangle(self):
-         vob = glGenVertexArrays(1)
-         glBindVertexArray(vob)
-
-         vb = glGenBuffers(1)
-         glBindBuffer(GL_ARRAY_BUFFER, vb)
-
-         pos = glGetAttribLocation(self.shader_prog, 'pos')
-         glEnableVertexAttribArray(pos)
-
-         glVertexAttribPointer(pos,3,GL_FLOAT,False, 0, ctypes.c_void_p(0))
-
-         vertices = np.array([-0.6, -0.6,0.0,
-                           0.0,0.6,0.0,
-                           0.6,-0.6,0.0,
-                           0.9,-0.1,0.0
-                           ], dtype=np.float32)
-         glBufferData(GL_ARRAY_BUFFER, 96, vertices, GL_STATIC_DRAW)
-         glBindVertexArray(0)
-         glDisableVertexAttribArray(pos)
-         glBindBuffer(GL_ARRAY_BUFFER,0)
-         self.display(vob)
-
-   def display(self, vert):
-      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-      glUseProgram(self.shader_prog)
-      glBindVertexArray(vert)
-      glDrawArrays(GL_TRIANGLES, 0, 3)
-      glDrawArrays(GL_TRIANGLES, 4, 3)
-      glBindVertexArray(0)
-      glUseProgram(0)
+class ListBoxRowWithData(Gtk.ListBoxRow):
+   def __init__(self,data):
+      super(Gtk.ListBoxRow,self).__init__()
+      self.data = data
+      self.add(Gtk.Label(data))
